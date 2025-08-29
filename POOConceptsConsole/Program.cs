c848e73f136e397cc5df1fdbedcd4fec61206125
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-08-29 15:51:31 -0500

    Finish class Date

---
+++ POOConceptsConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using POOConceptsCore;
+
+
+try
+{
+    var date1 = new Date(2025, 04, 31);
+    var date2 = new Date(2028, 2, 29);
+
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+
+
+
+
+
+
+
